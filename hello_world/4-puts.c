5a3e9e826b4a1b650281adaf952ec674ae063c83
root <9803@holbertonstudents.com>
2024-10-21 10:37:10 +0000

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-/* Fonction main */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	/* Fontion permettant d'afficher la phrase suivante */
