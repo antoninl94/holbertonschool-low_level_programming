e96ed4d05149c16c1c2830c54ad77f3dec1f92b4
root <9803@holbertonstudents.com>
2024-10-21 10:30:59 +0000

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
-/* Fonction principale */
-int main(void)
+int main(void) /* Fonction */
 {
 	/* Fontion permettant d'afficher la phrase suivante */
 	puts("\"Programming is like building a multilingual puzzle");
