4feaacd3769c7ae7ca6ff069fcde54bf18301a1e
root <9803@holbertonstudents.com>
2024-10-22 09:54:38 +0000

    alphabet1

---
+++ variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch, ch1;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch1 = 'A'; ch1 <= 'Z'; ch1++)
+	{
+		putchar(ch1);
+	}
+	putchar('\n');
+	return (0);
+}
