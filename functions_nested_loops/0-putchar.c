52d401d7fe4ded8a9b2b379bf0c038a426a93875
root <9803@holbertonstudents.com>
2024-10-23 09:10:18 +0000

    _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * This program print _putchar
+ * p: print "_putchar"
+ * Return: Always 0 (Success)
+ */
+
+void p(void) /* P: print "-putchar" */
+{
+	printf("_putchar\n");
+}
+
+int main(void)
+{
+	p();
+
+	return (0);
+}
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory will contain the file of the project 2
