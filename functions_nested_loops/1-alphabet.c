b90f1ab74de1075abf3403424d76897d76ba0062
root <9803@holbertonstudents.com>
2024-10-24 12:22:27 +0000

    alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,15 +1,15 @@
+#include "main.h"
 /**
  * print_alphabet - function to print alphabet
  * @ch: argument
- * Return: Always 0 (Success)
+ * Return: nothing
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	int ch;
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		_putchar(ch);
 	}
-	_putchar('\n')
-	return (0);
+	_putchar('\n');
 }
