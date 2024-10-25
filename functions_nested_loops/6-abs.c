8b1b8e71e5dadcb837a60b7d4d4673a233439cc7
root <9803@holbertonstudents.com>
2024-10-25 08:04:26 +0000

    abs

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,9 +1,21 @@
 #include "main.h"
 
 /**
+ * _abs - print the absolute value of an integer
+ * @c: the character
  *
+ * Return: -c is the negation of the nagitive result
+ * (wich make it positive)
+ * otherwice the result will be the number itself
  */
-int _abs(int)
+int _abs(int c)
 {
-
+	if (c < 0)
+	{
+		return (-c);
+	}
+	else
+	{
+		return (c);
+	}
 }
