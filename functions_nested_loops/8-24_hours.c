355fba54b248262b7a35031ecd740f444a0f0803
root <9803@holbertonstudents.com>
2024-10-25 19:53:10 +0000

    jack bauer

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print all the minutes un a day
+ */
+void jack_bauer(void)
+{
+	int heure, minute;
+
+	for (heure = 0; heure < 24; heure++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar(heure / 10 + '0');
+			_putchar(heure % 10 + '0');
+			_putchar(':');
+			_putchar(minute / 10 + '0');
+			_putchar(minute % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
