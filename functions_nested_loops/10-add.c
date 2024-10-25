d209f1ea616967984e131900475de43c7e345d48
root <9803@holbertonstudents.com>
2024-10-25 20:22:04 +0000

    add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - return the sum of num1 and num2
+ * @num1: first num to add
+ * @num2: second num to add
+ * Return: Result
+ */
+
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum = num1 + num2;
+
+	return (sum);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -10,5 +10,7 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int n);
 void jack_bauer(void);
+void times_table(void);
+int add(int num1, int num2);
 
 #endif
