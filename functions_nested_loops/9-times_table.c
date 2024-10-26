c7d483225a945dfdcacac76a6373028dfff99246
root <9803@hombertonstudents.com>
2024-10-26 12:16:38 +0000

    table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,16 +1,33 @@
 #include "main.h"
 
 /**
- *
- *
+ * times_table - print the 9x9 table
  */
 void times_table(void)
 {
-	int i, j;
+	int i;
+	int j;
+	int num;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0 ; i <= 9; i++)
 	{
-		for (j = 0; j < 9; j++);
+		_putchar('0');
+		for (j = 1; j <= 9; j++)
+		{
+			num = i * j;
+			_putchar(',');
+			_putchar(' ');
+			if (num <= 9)
+			{
+				_putchar(' ');
+				_putchar(num + '0');
+			}
+			else
+			{
+				_putchar((num / 10) + '0');
+				_putchar((num % 10) + '0');
+			}
+		}
+	_putchar('\n');
 	}
-	
 }
