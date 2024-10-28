1a45405e746fd618736bfdc82543df49f88cf22c
root <9803@holbertonstudents.com>
2024-10-28 15:42:49 +0000

    square

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -4,21 +4,21 @@
  * print_diagonal - print a diagonal depending on n
  * @n: number of lines
  */
-void print_diagonal(int n)
+void print_square(int size)
 {
 	int i, j;
 
-	if (n <= 0)
+	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for (i = 0; i < n; i++)
+		for (i = 0; i < size; i++)
 		{
-			for (j = 0; j < i; j++)
+			for (j = 0; j < size; j++)
 			{
-				_putchar(32);
+				_putchar(92);
 			}
 		_putchar(92);
 		_putchar('\n');
