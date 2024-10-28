9d0b799950f7f0dbb7592066e9e0507f9efc7f91
root <9803@holbertonstudents.com>
2024-10-28 09:27:37 +0000

    print numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - will print digit (from 0 to 9)
+ */
+void print_numbers(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		_putchar(d);
+	}
+	_putchar('\n');
+}
