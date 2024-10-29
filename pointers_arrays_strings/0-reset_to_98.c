62285ef74da293f2ab89b8a78ac27708260d929c
root <9803@holbertonstudents.com>
2024-10-29 15:34:04 +0000

    swap two integers

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - This function will take a pointer to a int
+ * as parameter
+ * @n: the int that be pointed
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
