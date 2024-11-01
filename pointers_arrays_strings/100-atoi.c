c05dfecfe72dac11490f8ce69e11fa0265a1a33f
root <9803@holbertonstudents.com>
2024-11-01 11:48:59 +0000

    atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,18 +1,43 @@
 #include "main.h"
 
 /**
- *
+ * _atoi - Convert string into integer
+ * @s: the string to convert
  */
 int _atoi(char *s)
 {
 	int i;
-
-	while (s[i] != '\0')
+	int sign;
+	int num;
+	
+	i = 0;
+	sign = 1;
+	num = 0;
+	
+	/* increment if s[i] == space */
+	while (s[i] == ' ')
 	{
 		i++;
-		if (s[i] >= 0 && s[i] <= 9)
-		{
-		}
 	}
-	return (s[i]);
+	/* manage the letters */
+	while (s[i] >= 'a' && s[i] <= 'z')
+	{
+		i++;
+	}
+	/* manage '-' and '+' signs */
+	while (s[i] == '+' || s[i] == '-')
+	{
+		if (s[i] == '-')
+		{
+			sign = sign * -1;
+		}
+		i++;
+	}
+	/* display intgers */
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		num = num * 10 + (s[i] - '0');
+		i++;
+	}
+	return num * sign;
 }
