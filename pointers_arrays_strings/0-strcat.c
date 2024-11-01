76d26c18491393ff22ab4a1e779f99bae02fe6c9
root <9803@holbertonstudents.com>
2024-11-01 14:53:57 +0000

    strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -9,10 +9,11 @@ char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
-	for (i = 0; src[i] != '\0'; i++);
+	for (i = 0; src[i] != '\0' - 1; i++);
 	for (j = 0; dest[j] != '\0'; j++, i++)
 	{
 		src[i] = dest[j];
 	}
+	src[i] = '\0';
 	return (dest);
 }
