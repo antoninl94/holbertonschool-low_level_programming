c1ee72fe0f2fa0091b98d87c78b0dad20ca6361a
root <9803@holbertonstudents.com>
2024-11-01 12:27:26 +0000

    atoi add condition if integers or letters

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -3,41 +3,54 @@
 /**
  * _atoi - Convert string into integer
  * @s: the string to convert
+ * Return: result of converting
  */
 int _atoi(char *s)
 {
 	int i;
 	int sign;
 	int num;
+	int let;
 	
 	i = 0;
 	sign = 1;
 	num = 0;
+	let = 0;
 	
-	/* increment if s[i] == space */
-	while (s[i] == ' ')
+	while (s[i] != '\0')
 	{
-		i++;
-	}
-	/* manage the letters */
-	while (s[i] >= 'a' && s[i] <= 'z')
-	{
-		i++;
-	}
-	/* manage '-' and '+' signs */
-	while (s[i] == '+' || s[i] == '-')
-	{
-		if (s[i] == '-')
+	/* increment if s[i] == space or == letters */
+		if (s[i] == ' ')
 		{
-			sign = sign * -1;
+			i++;
 		}
-		i++;
-	}
-	/* display intgers */
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		num = num * 10 + (s[i] - '0');
-		i++;
+	/* manage '-' and '+' signs */
+		else if (s[i] == '+' || s[i] == '-')
+		{
+			if (s[i] == '-')
+			{
+				sign = sign * -1;
+			}
+			i++;
+		}
+	/* display intgers if integer turn let = 1 (true) */
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			num = num * 10 + (s[i] - '0');
+			let = 1;
+			i++;
+		}
+		else
+		{
+			if (let == 1)
+			{
+				break;
+			}
+			else
+			{
+				i++;
+			}
+		}
 	}
 	return num * sign;
 }
