18af6a81730a904b9e5a8570ae479e68e020bd04
root <9803@holbertonstudents.com>
2024-11-01 14:37:50 +0000

    strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate two strings
+ * @src: the string to be appended
+ * @dest : the string to append
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; src[i] != '\0'; i++);
+	for (j = 0; dest[j] != '\0'; j++, i++)
+	{
+		src[i] = dest[j];
+	}
+	return (dest);
+}
