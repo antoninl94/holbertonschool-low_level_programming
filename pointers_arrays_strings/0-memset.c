574f639a4b44707f5b5729793ddc6e838e0ca0be
antoninl94 <9803@holbertonstudents.com>
2024-11-04 13:48:12 +0100

    memset first try

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - replace value of n bytes in *s
+ * @s: the string to be modified
+ * @b: the value to insert in *s
+ * @n: the number of bytes to be modified
+ * Return: the modified array s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char i = 0;
+
+	while (i != n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
