5c921123bae4c48496055eefc2fd99a340a8bd92
antoninl94 <9803@holbertonstudents.com>
2024-11-05 10:30:09 +0100

    strstr

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strstr - locate a substring
+ * @haystack: the string to be compared
+ * @needle: the substring to find in *haystack
+ * Return: needle if substring found otherwhise NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	while (*needle != '\0')
+	{
+		for (i = 0 ; haystack[i] != '\0'; i++)
+		if (*needle == haystack[i])
+		{
+			return (needle);
+		}
+		needle++;
+	}
+	return ('\0');
+}
