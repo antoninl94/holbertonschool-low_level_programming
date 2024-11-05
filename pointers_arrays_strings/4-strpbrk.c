b54eec2e130d2e4d784a4e551a78708468f1efb9
antoninl94 <9803@holbertonstudents.com>
2024-11-05 09:57:12 +0100

    strpbrk first try

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strpbrk - locate a char in a string
+ * @accept: the string to compare
+ * @s: the string to be compared
+ * return: s at the first occurence in *s otherwise return NULL
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int j;
+
+	while (*s != '\0')
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		if (accept[j] == *s)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return ('\0');
+}
