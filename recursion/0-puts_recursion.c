1331098a9b00a725fe3abfc3cd88585860546a28
antoninl94 <9803@holbertonstudents.com>
2024-11-06 18:09:00 +0100

    puts recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -7,12 +7,11 @@
 
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
-	s++;
-	if (*s != '\0')
-	{
-		_puts_recursion(s);
-	}
 	if (*s == '\0')
+	{
 		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
