2b22e0693ba27c65d0bd1a6a255a52b1edf02d78
antoninl94 <9803@holbertonstudents.com>
2024-11-06 13:22:34 +0100

    diagsums adding second loop (not correct)

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -11,13 +11,16 @@
 void print_diagsums(int *a, int size)
 {
 	int i;
-	int sum1;
-	int *ptr = a;
+	int sum1 = 0;
+	int sum2 = 0;
 
 	for (i= 0; i < size; i++)
 	{
-		sum1 += *(ptr + i * size + i);
+		sum1 += *(a + i * size + i);
 	}
-
-	printf("%d\n", sum1);
+	for (i = size - 1; i > 0; i--)
+	{
+		sum2 += *(a + i * (size - 1 - i));
+	}
+	printf("%d, %d\n", sum1, sum2);
 }
