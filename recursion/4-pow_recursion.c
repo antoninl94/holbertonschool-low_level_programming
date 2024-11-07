e056ccdb2f213029d23bd75d28116e6c35626f00
antoninl94 <9803@holbertonstudents.com>
2024-11-07 15:16:40 +0100

    pow recursion adding description

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,7 +1,10 @@
 #include "main.h"
 
 /**
- * 
+ * _pow_recursion - will return result of x power y
+ * @x: integer to multiply by itself
+ * @y: how many time multiply x
+ * Return: result of x power y
  */
 
 int _pow_recursion(int x, int y)
