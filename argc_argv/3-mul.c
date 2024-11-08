da76e8f69ca0333b9a7cf0174f6be49e0bcbe9b9
antoninl94 <9803@holbertonstudents.com>
2024-11-08 19:28:59 +0100

    mul first try

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,7 +12,12 @@ int main(int argc, char *argv[])
 {
 	int i, mul = 1;
 
-	if (argc > 1)
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else if (argc > 1)
 		for (i = 1; i < argc; i++)
 		{
 			mul = mul * atoi(argv[i]);
