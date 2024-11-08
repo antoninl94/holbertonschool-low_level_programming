4595def6a685bd85b28bb9708f586fecd4f71135
antoninl94 <9803@holbertonstudents.com>
2024-11-08 15:30:41 +0100

    whatsmyname first try

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - argc argv
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0 (Success)
+ */
+
+int main (int argc, char *argv[])
+{
+	while (argc--)
+	{
+		printf("%s\n", argv[0]++);
+	}
+	return (0);
+}
