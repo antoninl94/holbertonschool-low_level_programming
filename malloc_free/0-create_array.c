25e12696149463b2089c4e072c3b2af4c4ce32fa
antoninl94 <9803@holbertonstudents.com>
2024-11-12 09:20:47 +0100

    create array second try, adding correction

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -13,13 +13,14 @@ char *create_array(unsigned int size, char c)
 	char *ar;
 	unsigned int i;
 
-	ar = malloc(size * sizeof(char) * size);
-	ar[size] = c;
-
+	if (size == 0)
+		return (NULL);
+	ar = malloc(size * sizeof(char));
+	if (ar == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
-		if (size == 0)
-			return (NULL);
+		ar[i] = c;
 	}
 
 	return (ar);
