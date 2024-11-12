289f141adec866895278b9fc219309d07a29b974
antoninl94 <9803@holbertonstudents.com>
2024-11-12 10:06:21 +0100

    strdup second try, adding a code to calculate string length

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -12,12 +12,20 @@ char *_strdup(char *str)
 {
 	int len = 0;
 	char *ptr;
+	int i;
 
-	ptr = malloc(len * sizeof(str));
 	if (str == NULL)
 		return (NULL);
-	for (; str[len] != '\0'; len++)
-		ptr[len] = str[len];
+
+	while (str[len] != '\0')
+		len++;
+
+	ptr = malloc((len + 1) * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+		ptr[i] = str[i];
 
 	return (ptr);
 }
