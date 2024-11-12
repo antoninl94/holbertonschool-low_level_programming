09a841491440e962ebf01491a7a4c564e8149c68
antoninl94 <9803@holbertonstudents.com>
2024-11-12 13:49:38 +0100

    str_concat adding concatenation

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,9 +13,9 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 	int i = 0;
 	int j = 0;
-
-	if ((s1 == NULL && s2 == NULL) || s1 == NULL || s2 == NULL)
-		return (NULL);
+	int k = 0;
+	int l = 0;
+	int m = 0;
 
 	while (s1[i] != '\0')
 		i++;
@@ -25,8 +25,18 @@ char *str_concat(char *s1, char *s2)
 	ptr = malloc(sizeof(char) * (i + j + 1));
 	if (ptr == NULL)
 		return (NULL);
-
-	ptr = s1 + s2[j];
+	while (s1[m] != '\0')
+	{
+		ptr[k] = s1[m];
+		k++;
+		m++;
+	}
+	while (s2[l] != '\0')
+	{
+		ptr[k] = s2[l];
+		l++;
+		k++;
+	}
 
 	return (ptr);
 }
