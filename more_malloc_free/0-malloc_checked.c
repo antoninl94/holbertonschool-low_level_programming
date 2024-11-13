930b3e6a0ba2eef2d25e60e8c799c2a81da73c6f
antoninl94 <9803@holbertonstudents.com>
2024-11-13 10:20:23 +0100

    malloc checked first try

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -4,6 +4,7 @@
 /**
  * malloc_checked - allcate memory
  * @b: the variable type that must be allocated
+ * Return: pointer to allocated memory
  */
 
 void *malloc_checked(unsigned int b)
@@ -14,6 +15,6 @@ void *malloc_checked(unsigned int b)
 
 	if (i == NULL)
 		exit(98);
-	
+
 	return (i);
 }
