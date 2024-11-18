e7b07e5fef9d46719e4ae666e0a969eb4edbbc06
antoninl94 <9803@holbertonstudents.com>
2024-11-18 15:55:30 +0100

    int index creation

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - return the index of the first element
+ * for wich the cmp function does not return 0
+ * @array: the array
+ * @size: size of the array
+ * @cmp: the function used to compare array
+ * Return: -1 if error otherwise return index
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0)
+		return (-1);
+	while (i < size)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
