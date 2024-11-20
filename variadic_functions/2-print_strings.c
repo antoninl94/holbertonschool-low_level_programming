c238ea96e5334b142a89d664d978bc766f8bb76a
antoninl94 <9803@holbertonstudents.com>
2024-11-20 18:18:52 +0100

    2-print_strings.c adding pointer to char initialized to va_arg

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -13,11 +13,16 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i = 0;
+	char *str;
 
 	va_start(args, n);
 	while (i < n)
 	{
-		printf("%s", va_arg(args, char*));
+		str = va_arg(args, char*);
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
 		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 		i++;
