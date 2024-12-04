859376c8bb75d90fa596799f1bb3d44dccbb6bd1
antoninl94 <9803@holbertonstudents.com>
2024-12-04 10:56:34 +0100

    add_dnodeint and add_dnodeint_end adding head==NULL condition

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -13,9 +13,19 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-	new->n = n;
-	new->next = *head;
 	new->prev = NULL;
-	*head = new;
+	new->n = n;
+	if (*head == NULL)
+	{
+		new->next = NULL;
+		*head = new;
+	}
+	else
+	{
+		new->next = *head;
+		(*head)->prev = new;
+		*head = new;
+	}
+
 	return (new);
 }
