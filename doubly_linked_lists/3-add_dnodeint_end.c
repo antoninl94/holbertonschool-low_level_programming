31d259b66d9899bd544c76044e8919b9a912b0fd
antoninl94 <9803@holbertonstudents.com>
2024-12-04 11:47:26 +0100

    fixing description

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * add_dnodeint_ent - add a new node at the end of a list
+ * add_dnodeint_end - add a new node at the end of a list
  * @head: point on the beginning of a list
  * @n: the int in the node
  * Return: adress of the new element or NULL if it failed
